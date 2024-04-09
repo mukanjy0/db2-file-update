25f6c4676525b4085871f39197d154a476811946
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2024-04-09 11:27:44 -0500

    fixed methods for moved to last implementation, still not persistant

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <exception>
+#define sz(x) (int)x.size()
 using namespace std;
 
 struct Student {
@@ -11,11 +12,16 @@ struct Student {
     char career[15];
     int semester;
     float monthlyPayment;
+    friend ostream& operator<<(ostream& out, const Student& s) {
+        out << s.code << '\n';
+        out << s.surname << ' ' << s.lastName << '\n';
+        out << s.career << " - " << s.semester << " | " << s.monthlyPayment << '\n';
+        return out;
+    }
 };
 
 class FixedRecord {
     char* filename;
-    char* mode;
     bool freeList;
 
     vector<Student> loadMTL() {
@@ -26,8 +32,7 @@ class FixedRecord {
         // read & process header
         int n {}; // number of records
         f.read((char *) &n, sizeof(n));
-        char aux[sizeof(s)-sizeof(n)];
-        f.read((char *) &aux, sizeof(aux));
+        f.seekg(sizeof(s)-sizeof(n), ios::cur);
 
         // read records
         for (int i = 0; i < n; ++i) {
@@ -49,7 +54,7 @@ class FixedRecord {
         // read & process header
         int n {}; // number of records
         f.read((char *) &n, sizeof(n));
-        if (pos > n)
+        if (++pos > n)
             throw runtime_error("Record does not exist.");
 
         // read record
@@ -61,19 +66,20 @@ class FixedRecord {
     }
 
     void addMTL(Student s) {
-        fstream f (filename, ios::in | ios::out | ios::binary | ios::ate);
-
-        // add record
-        f.write((char *) &s, sizeof(s));
+        fstream f (filename, ios::in | ios::out | ios::binary);
 
         // read & process header
         int n {}; // number of records
         f.read((char *) &n, sizeof(n));
 
-        // update header
+        // add record
         ++n;
-        f.seekg(0,ios::beg);
-        f.write((char *) &n, sizeof(s));
+        f.seekp(n * (int)sizeof(s), ios::beg);
+        f.write((char *) &s, sizeof(s));
+
+        // update header
+        f.seekp(0, ios::beg);
+        f.write((char *) &n, sizeof(n));
 
         f.close();
     }
@@ -85,21 +91,21 @@ class FixedRecord {
         // read & process header
         int n {}; // number of records
         f.read((char *) &n, sizeof(n));
-        if (pos > n)
+        if (++pos > n)
             throw runtime_error("Record does not exist.");
 
         // read last record
-        f.seekg(n*(int)sizeof(s), ios::beg);
+        f.seekg(n * (int)sizeof(s), ios::beg);
         f.read((char *) &s, sizeof(s));
 
         // replace toRemove record
-        f.seekp(n*(int)sizeof(s), ios::beg);
+        f.seekp(pos * (int)sizeof(s), ios::beg);
         f.write((char *) &s, sizeof(s));
 
         // update header
         --n;
-        f.seekp(0,ios::beg);
-        f.write((char *) &n, sizeof(s));
+        f.seekp(0, ios::beg);
+        f.write((char *) &n, sizeof(n));
 
         f.close();
     }
@@ -129,13 +135,20 @@ class FixedRecord {
 public:
     FixedRecord(char* filename, const char* mode) : filename(filename) {
         freeList = mode[0] == 'F';
+        ifstream fr (filename, ios::binary);
+        streampos begin = fr.tellg();
+        fr.close();
+
         ofstream f (filename, ios::binary);
-        int n = 0;
-        Student s{};
-        f.write((char *) &n, sizeof(s));
+        if (begin < 0) {
+            int n {};
+            Student s{};
+            f.write((char *) &n, sizeof(s));
+        }
+
         f.close();
     }
-    vector<Student> load() { return freeList ? loadFL() : loadMTL(); }
+    vector<Student> load() { return loadMTL(); }
     void add(Student record) { return addMTL(record); }
     //void add(Student record) { return freeList ? addFL(record) : addMTL(record); }
     Student readRecord(int pos) { return readMTL(pos); }
@@ -144,7 +157,59 @@ public:
     //void remove(int pos) { return freeList ? removeFL(pos) : removeMTL(pos); }
 };
 
+void testMTL() {
+    Student students[10] = {
+            {"A001", "Ana", "Perez", "INF", 1, 1200.50},
+            {"A002", "Luis", "Gomez", "ADM", 2, 1100.00},
+            {"A003", "Mia", "Lopez", "DER", 3, 1300.75},
+            {"A004", "Juan", "Diaz", "MED", 4, 1400.25},
+            {"A005", "Sara", "Mora", "PSI", 1, 1250.00},
+            {"A006", "Leo", "Vega", "ARQ", 2, 1350.45},
+            {"A007", "Rita", "Solis", "CIV", 3, 1450.60},
+            {"A008", "Tito", "Luna", "IND", 4, 1550.20},
+            {"A009", "Lola", "Nuez", "INF", 1, 1150.30},
+            {"A010", "Paco", "Rio", "ADM", 2, 1050.90}
+    };
+
+    FixedRecord fr ("data.bin", "MOVE_THE_LAST");
+    for (const auto& s : students)
+        fr.add(s);
+    auto v = fr.load();
+
+    int n = sz(v);
+    for (int i = 0; i < n; ++i)
+        cout << "Student " << i << ":\n" << v[i] << "-----------------------\n";
+
+    v[0] = fr.readRecord(0);
+    cout << "Student " << 0 << ":\n" << v[0] << "-----------------------\n";
+    v[9] = fr.readRecord(9);
+    cout << "Student " << 9 << ":\n" << v[9] << "-----------------------\n";
+
+    fr.remove(0);
+    v[9] = fr.readRecord(0);
+    cout << "Student " << 9 << ":\n" << v[9] << "-----------------------\n";
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //testMTL();
+    char name[] = "data.bin";
+    char* filename = name;
+
+    ofstream of (filename, ios::binary);
+    of.close();
+
+    ifstream f (filename, ios::binary);
+    int n {};
+    f.read((char *) &n, sizeof(int));
+    cout << n << '\n';
+//    streampos begin = f.tellg();
+//    cout << begin << '\n';
+//    f.close();
+
+//    if (begin < 0) {
+//        int n {};
+//        Student s{};
+//        f.write((char *) &n, sizeof(s));
+//    }
     return 0;
 }
