ec7dd5cd3639959039a2fbc5902e64c09bb4a199
Mukanjyo <122047216+mukanjy0@users.noreply.github.com>
2024-04-11 08:49:59 -0500

    added full solution to problem 3 (bug so not fully functional)

--- p3.cpp
+++ p3.cpp
@@ -68,64 +68,8 @@ class VariableRecordB {
         f.close();
     }
 
-public:
-    VariableRecordB(const char* filename) {
-        this->filename = new char[strlen(filename) + 1];
-        strcpy(this->filename, filename);
-
-        ofstream f (filename, ios::in | ios::binary);
-        if (!f.is_open()) {
-            f.clear();
-            f.open(filename, ios::binary);
-            setHeader(f);
-            setHeaderMetadata();
-        }
-        f.close();
-    }
-    vector<Enrollment> load() {
-
-    }
-    void add(Enrollment record) {
-        // update main file
-        ofstream f (filename, ios::app | ios::ate | ios::binary);
-        int end {(int)f.tellp()};
-
-        int sz {(int)record.code.size()};
-        f.write(reinterpret_cast<char*>(&(sz)), sizeof(int));
-        f.write(record.code.c_str(), sz);
-
-        sz = sizeof(int);
-        f.write(reinterpret_cast<char*>(&(sz)), sizeof(int));
-        f.write(reinterpret_cast<char*>(&(record.semester)), sz);
-
-        sz = sizeof(float);
-        f.write(reinterpret_cast<char*>(&(sz)), sizeof(int));
-        f.write(reinterpret_cast<char*>(&(record.monthlyPayment)), sz);
-
-        sz = (int)record.observations.size();
-        f.write(reinterpret_cast<char*>(&(sz)), sizeof(int));
-        f.write(record.observations.c_str(), sz);
-
-        f.close();
-
-        // update metadata
-        //int recordSz {5 * (int)sizeof(int) + (int)sizeof(record.code) + (int)sizeof(float) + (int)sizeof(record.observations)};
-        f.open(metadataFilename, ios::app | ios::binary);
-        f.write(reinterpret_cast<char*>(&end), sizeof(int));
-        f.close();
-    }
-    Enrollment readRecord(int pos) {
-        ifstream f (metadataFilename, ios::binary);
-        f.seekg(++pos * (int)sizeof(int));
-        int start {};
-        f.read(reinterpret_cast<char*>(&start), sizeof(int));
-        f.close();
-
-        f.open(filename, ios::binary);
-        f.seekg(start);
-
+    static bool readEnrollment(istream& f, Enrollment& e)  {
         int sz {};
-        Enrollment e {};
 
         f.read(reinterpret_cast<char*>(&sz), sizeof(int));
         char* aux = new char[sz];
@@ -146,7 +90,89 @@ public:
         e.observations = string(aux);
         delete[] aux;
 
-        return e;
+        return f.good();
+    }
+
+    static void writeEnrollment(ostream& f, Enrollment& e) {
+        int sz {(int)e.code.size()};
+        f.write(reinterpret_cast<char*>(&(sz)), sizeof(int));
+        f.write(e.code.c_str(), sz);
+
+        sz = sizeof(int);
+        f.write(reinterpret_cast<char*>(&(sz)), sizeof(int));
+        f.write(reinterpret_cast<char*>(&(e.semester)), sz);
+
+        sz = sizeof(float);
+        f.write(reinterpret_cast<char*>(&(sz)), sizeof(int));
+        f.write(reinterpret_cast<char*>(&(e.monthlyPayment)), sz);
+
+        sz = (int)e.observations.size();
+        f.write(reinterpret_cast<char*>(&(sz)), sizeof(int));
+        f.write(e.observations.c_str(), sz);
+    }
+
+public:
+    explicit VariableRecordB(const char* filename) {
+        this->filename = new char[strlen(filename) + 1];
+        strcpy(this->filename, filename);
+
+        ofstream f (filename, ios::in | ios::binary);
+        if (!f.is_open()) {
+            f.clear();
+            f.open(filename, ios::binary);
+            setHeader(f);
+            setHeaderMetadata();
+        }
+        f.close();
+    }
+    vector<Enrollment> load() {
+        ifstream f (metadataFilename, ios::binary);
+        f.seekg(sizeof(int));
+        int start {};
+        f.read(reinterpret_cast<char*>(&start), sizeof(int));
+        f.close();
+
+        vector<Enrollment> enrollments;
+        Enrollment e {};
+
+        f.open(filename, ios::binary);
+        f.seekg(start);
+        while (readEnrollment(f, e)) {
+            enrollments.push_back(e);
+        }
+        f.close();
+
+        return enrollments;
+    }
+    void add(Enrollment record) {
+        // update main file
+        ofstream f (filename, ios::app | ios::ate | ios::binary);
+        int end {(int)f.tellp()};
+        writeEnrollment(f, record);
+        f.close();
+
+        // update metadata
+        //int recordSz {5 * (int)sizeof(int) + (int)sizeof(record.code) + (int)sizeof(float) + (int)sizeof(record.observations)};
+        f.open(metadataFilename, ios::app | ios::binary);
+        f.write(reinterpret_cast<char*>(&end), sizeof(int));
+        f.close();
+    }
+    Enrollment readRecord(int pos) {
+        ifstream f (metadataFilename, ios::binary);
+        f.seekg(++pos * (int)sizeof(int));
+        int start {};
+        f.read(reinterpret_cast<char*>(&start), sizeof(int));
+        f.close();
+
+        f.open(filename, ios::binary);
+        f.seekg(start);
+
+        Enrollment e {};
+        bool good = readEnrollment(f, e);
+        f.close();
+
+        if (good) return e;
+        throw runtime_error("Could not read record at position " + to_string(pos) + ".");
     }
 };
 
@@ -165,17 +191,26 @@ void test() {
             {"CS2027", 8, 1200.50, "Must take CS2019 first."},
             {"CS2028", 9, 1200.50, "Must take CS2019 first."}
     };
-    for (const auto& e : enrollments)
-        vr.add(e);
+//    for (const auto& e : enrollments)
+//        vr.add(e);
 
     // test load
 //    vector<Enrollment> v = vr.load();
 //    for (int i = 0; i < (int)v.size(); ++i)
-//        assert(enrollments[i] == v[i]);
+//        if (enrollments[i] != v[i]) {
+//            cout << enrollments[i] << "----------------------------\n";
+//            cout << v[i] << "===========================\n";
+//        }
+        //assert(enrollments[i] == v[i]);
 
     // test read
-    for (int i = 0; i < 9; ++i)
-        assert(enrollments[i] == vr.readRecord(i));
+    for (int i = 0; i < 9; ++i) {
+        if (enrollments[i] != vr.readRecord(i)) {
+            cout << enrollments[i] << "----------------------------\n";
+            cout << vr.readRecord(i) << "===========================\n";
+        }
+    }
+        //assert(enrollments[i] == vr.readRecord(i));
 }
 
 int main() {
